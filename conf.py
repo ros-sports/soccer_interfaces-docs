64aaef22431e3e14b1024d119b119cded31aa0be
Kenji Brameld <kenjibrameld@gmail.com>
2022-04-14 09:16:01 +0900

    custom settings and logo in conf.py
    Signed-off-by: Kenji Brameld <kenjibrameld@gmail.com>

--- conf.py
+++ conf.py
@@ -30,9 +30,6 @@ author = 'ROS Sports'
 extensions = [
 ]
 
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
-
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
@@ -44,9 +41,6 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_logo = 'images/logo.png'
